1bee498e734ae267c666bc6447393521712235b7
danielabadgamero <danielabadgamero@gmail.com>
2022-05-03 18:58:47 +0200

    Added more authors and their images

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const cards = [...document.querySelectorAll(".card")];
+const authors = [];
 
 cards.forEach((card) => {
+    authors.push(document.createElement("h1"));
+    authors[authors.length - 1].textContent = card.id.toUpperCase().replace("-", " ");
+    card.appendChild(authors[authors.length - 1])
     card.children[0].setAttribute("src", `${card.id}.jpg`);
     card.addEventListener("mousedown", () => {
         location.href = `./gallery/${card.id}.html`
