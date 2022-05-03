8a64d9b676ed73e979155f9e2c7f3d10d8d089ff
danielabadgamero <danielabadgamero@gmail.com>
2022-05-03 18:10:19 +0200

    Added more authors

---
+++ script.js
@@ -0,0 +1,13 @@
+const cards = [...document.querySelectorAll(".card")];
+
+cards.forEach((card) => {
+    card.addEventListener("mousedown", () => {
+        location.href = `./gallery/${card.id}.html`
+    })
+    card.addEventListener("mouseenter", () => {
+        card.classList.add("card-hover");
+    })
+    card.addEventListener("mouseleave", () => {
+        card.classList.remove("card-hover")
+    })
+})
