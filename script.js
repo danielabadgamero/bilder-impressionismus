f6b369e8165852d64eab06510f770276e8a415a7
danielabadgamero <danielabadgamero@gmail.com>
2022-05-03 19:37:38 +0200

    Started adding author files

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const authors = [];
 
 cards.forEach((card) => {
     authors.push(document.createElement("h1"));
-    authors[authors.length - 1].textContent = card.id.toUpperCase().replace("-", " ");
+    authors[authors.length - 1].textContent = card.id.toUpperCase().replaceAll("-", " ");
     card.appendChild(authors[authors.length - 1])
     card.children[0].setAttribute("src", `${card.id}.jpg`);
     card.addEventListener("mousedown", () => {
